e695c2d01fc88457292f9434fcd4bdaf35ab91f4
Joao Guilherme <jgriquena@gmail.com>
2024-08-18 17:25:28 -0300

    .

---
+++ src/services/cart.js
@@ -0,0 +1,20 @@
+//quais ações meu carrinho pode fazer
+//1 - adicionar item
+//2 - remover item
+//3- calcular total
+
+async function addItem(userCart) {
+
+}
+
+async function deletItem(userCart, name) {
+
+}
+
+async function removeItem(userCart, index) {
+
+}
+
+async function calculateTotal(userCart) {
+
+}
