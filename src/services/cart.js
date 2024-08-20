d6513f0ddf8bcdbe79aa431f9ff27c55482817b1
Joao Guilherme <jgriquena@gmail.com>
2024-08-20 20:23:13 -0300

    display

--- src/services/cart.js
+++ src/services/cart.js
@@ -20,13 +20,23 @@ async function removeItem(userCart, index) {
 }
 
 async function calculateTotal(userCart) {
+    console.log("\n💸💸Shopee Cart TOTAL IS: ")
+
     const result = userCart.reduce((total, item) => total + item.subtotal(), 0)
-    console.log(result)
+    console.log(`Total: ${result}`)
+}
+
+async function displayCart(userCart) {
+    console.log("\n🛒🛒Shopee cart list: ")
+    userCart.forEach((item, index) => {
+        console.log(`${index + 1}. ${item.name} - R$ ${item.price} | ${item.quantity}x | Subtotal = ${item.subtotal()}`)
+    });
 }
 
 export {
     addItem,
     calculateTotal,
     deletItem,
-    removeItem
+    removeItem,
+    displayCart
 }
