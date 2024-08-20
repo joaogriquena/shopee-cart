9a5207243b63fc372f1b0878dc841e15c844e4ec
Joao Guilherme <jgriquena@gmail.com>
2024-08-19 22:21:41 -0300

    .

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
+import * as cartService from './services/cart.js'
 import createItem from "./services/item.js"
 
-const cart = [];
+const myCart = [];
+const myWhishList = [];
 
 console.log("Welcome to the your shopee Cart!")
 
 const item1 = await createItem("hotwheels ferrari", 20.99, 1)
-const item2 = await createItem("hotwheels lamborghini", 39.99, 3)
+const item2 = await createItem("hotwheels lamborghini", 39.99, 1)
 
-console.log(item2.subtotal())
+await cartService.addItem(myCart, item1)
+await cartService.addItem(myWhishList, item2)
+
+console.log("Shopee Cart TOTAL IS: ")
+
+await cartService.calculateTotal(myCart)
+await cartService.calculateTotal(myWhishList)
--- src/services/cart.js
+++ src/services/cart.js
@@ -3,8 +3,8 @@
 //2 - remover item
 //3- calcular total
 
-async function addItem(userCart) {
-
+async function addItem(userCart, item) {
+    userCart.push(item)
 }
 
 async function deletItem(userCart, name) {
@@ -16,5 +16,13 @@ async function removeItem(userCart, index) {
 }
 
 async function calculateTotal(userCart) {
+    const result = userCart.reduce((total, item) => total + item.subtotal(), 0)
+    console.log(result)
+}
 
+export {
+    addItem,
+    calculateTotal,
+    deletItem,
+    removeItem
 }
